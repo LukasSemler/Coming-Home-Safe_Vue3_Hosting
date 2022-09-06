6ba72445c7df2792dae89dea0ae37e120301e6db
LukasSemler <semler.l04@htlwienwest.at>
2022-09-06 11:46:05 +0200

    Versuch 1

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(ErrorHandler);
 app.use(NotFoundHandler);
 
 const http = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 wsServer(http);
