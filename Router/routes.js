bb13159bd5ef336061447b1c760500c63472e596
BenjaminStauf <stauf.b03@htlwienwest.at>
2022-12-03 17:35:39 +0100

    ajd mach jetzt

---
+++ Router/routes.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import {
+  sendCodeUser,
+  sendThumbnail,
+  sendDataRegister,
+  login,
+  sendPosition,
+  sendNewPassword,
+  patchUser,
+  deleteAccount,
+  changePassword,
+  getMitarbeiter,
+  changeRole,
+} from '../Controllers/kunde.js';
+
+const router = express.Router();
+
+// Test route
+router.get('/', (req, res) => res.status(200).send('Test'));
+
+//KundenRouten
+router.post('/sendNewPassword', asyncHandler(sendNewPassword));
+router.post('/sendCodeRegister', asyncHandler(sendCodeUser));
+router.post('/sendThumbnail', asyncHandler(sendThumbnail));
+router.post('/sendDataRegister', asyncHandler(sendDataRegister));
+router.post('/login', asyncHandler(login));
+router.patch('/patchUser/:id', asyncHandler(patchUser));
+
+router.post('/sendPosition', asyncHandler(sendPosition));
+
+router.delete('/deleteAccount/:id', asyncHandler(deleteAccount));
+
+router.patch('/changePassword/:id', asyncHandler(changePassword));
+
+router.get('/getMitarbeiter', asyncHandler(getMitarbeiter));
+router.patch('/changeRole/:id', asyncHandler(changeRole));
+
+router.get('/*', (req, res) => res.redirect('/'));
+
+export default router;
