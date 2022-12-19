f234f6fee82607490ac33c751a79c2b2b37fb3a1
BenjaminStauf <72499370+BenjaminStauf@users.noreply.github.com>
2022-12-19 22:35:07 +0100

    ABC-BÄR

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import morgan from 'morgan';
+
+import dotenv from 'dotenv';
+import path from 'path';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+import { ErrorHandler, NotFoundHandler } from './Middleware/index.js';
+import wsServer from './websockets/index.js';
+import wsclient from 'websocket';
+
+import routes from './Router/routes.js';
+
+dotenv.config();
+
+const dirname = path.resolve();
+const app = express();
+
+const PORT = process.env.PORT || 2410;
+
+app.use(morgan('dev'));
+// app.use(helmet());
+app.use(cors());
+app.use(fileUpload());
+app.use(express.static(path.join(dirname, '/public')));
+app.use(express.json());
+
+app.use('/', routes);
+
+app.use(ErrorHandler);
+app.use(NotFoundHandler);
+
+const http = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+//WebsocketServer
+wsServer(http);
