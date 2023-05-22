032510dcc4470bb9c77d512fd5f5a3e607e04c51
BenjaminStauf <stauf.b03@htlwienwest.at>
2023-05-22 12:24:09 +0200

    test

--- app.js
+++ app.js
@@ -25,10 +25,13 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(fileUpload());
 app.use(express.static(path.join(dirname, '/public')));
-app.use(express.json());
-app.use(bodyParser({ limit: '50mb' }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
