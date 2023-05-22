50d11756d44d7b8346fbcd438fe73c5d4e6cce28
BenjaminStauf <stauf.b03@htlwienwest.at>
2023-05-22 12:07:37 +0200

    http limit

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.use(fileUpload());
 app.use(express.static(path.join(dirname, '/public')));
 app.use(express.json());
 app.use(bodyParser({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use('/', routes);
 
